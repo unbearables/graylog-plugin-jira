d9e7f3bd0ef3fd778abd8abe40647f20d342e2a9
David Horak <horak.david@protonmail.com>
2023-02-23 15:05:52 +0100

    #40 support graylog 5

--- src/main/java/org/graylog/plugins/jira/JiraNotificationModule.java
+++ src/main/java/org/graylog/plugins/jira/JiraNotificationModule.java
@@ -2,6 +2,7 @@ package org.graylog.plugins.jira;
 
 import org.graylog.plugins.jira.event.notifications.JiraEventNotificationConfig;
 import org.graylog.plugins.jira.event.notifications.JiraEventNotification;
+import org.graylog.plugins.jira.event.notifications.JiraEventNotificationConfigEntity;
 import org.graylog2.plugin.PluginConfigBean;
 import org.graylog2.plugin.PluginModule;
 
@@ -17,7 +18,12 @@ public class JiraNotificationModule extends PluginModule {
 
     @Override
     protected void configure() {
-        addNotificationType(JiraEventNotificationConfig.TYPE_NAME, JiraEventNotificationConfig.class,
-                JiraEventNotification.class, JiraEventNotification.Factory.class);
+        addNotificationType(
+                JiraEventNotificationConfig.TYPE_NAME,
+                JiraEventNotificationConfig.class,
+                JiraEventNotification.class,
+                JiraEventNotification.Factory.class,
+                JiraEventNotificationConfigEntity.TYPE_NAME,
+                JiraEventNotificationConfigEntity.class);
     }
 }
