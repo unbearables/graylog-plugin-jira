c9d8b3629a2d55d7b5e9150672beb1f2b3c47ee3
David Horak <horak.david@protonmail.com>
2023-02-23 16:10:37 +0100

    40 support graylog 5 (#41)
    * #40 support graylog 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const PluginWebpackConfig = require('graylog-web-plugin').PluginWebpackConfig;
 const loadBuildConfig = require('graylog-web-plugin').loadBuildConfig;
 const path = require('path');
-const buildConfig = loadBuildConfig(path.resolve(__dirname, './build.config'));
 
-module.exports = new PluginWebpackConfig('org.graylog.plugins.jira.JiraNotificationPlugin', buildConfig, {
+module.exports = new PluginWebpackConfig(__dirname,
+    'org.graylog.plugins.jira.JiraNotificationPlugin',
+    loadBuildConfig(path.resolve(__dirname, './build.config')), {
   // Here goes your additional webpack configuration.
 });
